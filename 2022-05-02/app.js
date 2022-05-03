2f0cdbbe507764500c21e7d357c9d87d39cf612e
Ryan Hardin <rhardin378@gmail.com>
2022-05-02 21:06:59 -0400

    completed today's daily code challenge

--- 2022-05-02/app.js
+++ 2022-05-02/app.js
@@ -1,2 +1,17 @@
 // Daily Coding Challenge
 
+// Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+// Examples:
+
+// solution('abc', 'bc') // returns true
+// solution('abc', 'd') // returns false
+
+function solution(str, ending){
+    const endLength = ending.length
+    if (str.slice(-endLength) === ending || ending === '') {
+      return true
+    } else {
+      return false
+    }
+  }
