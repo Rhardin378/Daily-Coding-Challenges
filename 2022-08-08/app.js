f0a79e3e3ab09b10a52f874c8bacf9e813e7717d
Ryan Hardin <rhardin378@gmail.com>
2022-08-08 23:41:42 -0400

    changed conditional to check for length instead of undefined

--- 2022-08-08/app.js
+++ 2022-08-08/app.js
@@ -6,17 +6,17 @@
 
 // In languages that do not have an empty option, just return null
 
-//pseudo code:
-// i want to check for the last number in the array
-// this can be done by checking the last index of the passed in array xs[xs.length - 1] 
-// if the array is empty  it will return undefined when using [xs.length-1] i can then make 
-// a conditional  to check its undefined and return null if it isnt
-// xs[xs.length-1] == undefined ? null
-// else return xs[xs.length - 1]
+
 //Parameters: takes in an array of numbers or an empty array 
 //returns: returns the last element in the passed in array  or null if the array is empty
 //examples: last([])=> null
 // last([1,2,3]) => 3
 // last([4,5,2]) => 2
-
-const last = xs => xs[xs.length-1] == undefined ? null : xs[xs.length-1] 
+//pseudo code:
+// i want to check for the last number in the array
+// this can be done by checking the last index of the passed in array xs[xs.length - 1] 
+// if the array is empty it will have a length of 0 test this using xs.length i can then make 
+// a conditional  to check  and return null if it is 0 
+// xs.length == 0 ? null
+// else        : return xs[xs.length - 1]
+const last = xs => xs.length == 0 ? null : xs[xs.length-1] 
