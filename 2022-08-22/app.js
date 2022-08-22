f3378efc02121870ef4ee8452b2a45b5c0f2c66a
Ryan Hardin <rhardin378@gmail.com>
2022-08-22 18:47:20 -0400

    completed a coding challenge

---
+++ 2022-08-22/app.js
@@ -0,0 +1,20 @@
+// Very simple, given an integer or a floating-point number, find its opposite.
+
+
+// Parameters
+// the function is being passed a positive or negative number
+// returns 
+// returns the opposite of the number passed as an argument
+
+// examples:
+// 1: -1
+// 14: -14
+// -34: 34
+
+// pseudocode
+
+// if the number is less than 0 return the number * -1 to make it positive 
+// else make the number negative by multiplying times -1
+
+
+const opposite = number => number < 0 ? number * -1 : number * -1
