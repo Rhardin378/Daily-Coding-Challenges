62edd08eb1af99fe700095cf09d7c0a0dae414a9
Ryan Hardin <rhardin378@gmail.com>
2023-06-22 20:18:52 -0400

    completed coding challenge

---
+++ 06-22-2023/app.js
@@ -0,0 +1,11 @@
+  
+//   p: takes in a number (negative or positive)
+//   r: returns the negative of a number input
+//   e:
+//   makeNegative(1);    // return -1
+// makeNegative(-5);   // return -5
+// makeNegative(0);    // return 0
+//   p:
+//   if number is positve return num  else return num * -1
+  
+const makeNegative = num => num < 0 ? num : num * -1  
